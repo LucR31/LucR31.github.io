b74f03d15a8381c153e40eaf1983631a71ea4ee6
LucR31 <94859181+LucR31@users.noreply.github.com>
2025-07-04 15:43:01 +0200

    update

--- main.js
+++ main.js
@@ -1,41 +1,58 @@
 import * as THREE from 'three';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
-const loader = new GLTFLoader();
-const light = new THREE.DirectionalLight(0xffffff,1);
-light.position.set(2,2,2);
-
+const container = document.getElementById( 'card' );
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+scene.background = new THREE.Color( 0x000000 );
+scene.fog = new THREE.Fog( 0x99DDFF, 5000, 10000 );
+const camera = new THREE.PerspectiveCamera( 60, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+
 const renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth/2, window.innerHeight/2 );
+container.appendChild( renderer.domElement );
 
-renderer.setSize( window.innerWidth, window.innerHeight );
-renderer.setAnimationLoop( animate );
-document.body.appendChild( renderer.domElement );
+const pmremGenerator = new THREE.PMREMGenerator( renderer );
+//scene.environment = pmremGenerator.fromScene( new RoomEnvironment(), 0.04 ).texture;
 
-const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshBasicMaterial( { color: 'white' } );
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
-scene.add( light );
+camera.position.z = 40;
 
-camera.position.z = 5;
 
-loader.load( 'corona_1.glb', function ( gltf ) {
+const light = new THREE.DirectionalLight( 0xffffcc, 1 );
 
-	scene.add( gltf.scene );
+				light.position.set( 200, 1000, 50 );
+
+				light.castShadow = true;
+
+				light.shadow.camera.left = - 50000;
+				light.shadow.camera.right = 50000;
+				light.shadow.camera.top = 50000;
+				light.shadow.camera.bottom = - 50000;
+				light.shadow.camera.far = 20000;
+
+				light.shadow.bias = - 0.001;
+
+				light.shadow.camera.updateProjectionMatrix();
+
+				scene.add( light );
+        const hemi = new THREE.HemisphereLight( 0xffffcc, 0x669933, 1 / 3 );
+
+				scene.add( hemi );
+
+const loader = new GLTFLoader();
+loader.load( 'corona.glb', function ( gltf ) {
+  const model = gltf.scene; 
+  scene.add( model );
 
 }, undefined, function ( error ) {
 
-	console.error( error );
+  console.error( error );
 
 } );
 
 function animate() {
-
-	cube.rotation.x += 0.01;
-	cube.rotation.y += 0.01;
-
-	renderer.render( scene, camera );
-
+  scene.rotation.x += 0.0015;
+  scene.rotation.y += 0.0015;
+  renderer.render( scene, camera );
 }
+renderer.setAnimationLoop( animate );
